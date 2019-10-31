1e1975e5e0bb472e144077ed7d9f26071346b9eb
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-10-31 18:10:08 -0400

    PoRt

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ server.get('/', (req, res) => {
 
 const port = process.env.PORT||4000
 server.listen(port, () => {
-  console.log(`\n Server running on localhost: ${PORT} \n`);
+  console.log(`\n Server running on localhost: ${port} \n`);
 });
